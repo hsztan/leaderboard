be0e46516204afd7d191ee540f70b41e8f90d817
Henry Nawrocki <hdev@sztanski.com>
2022-04-06 14:21:44 -0500

    Submit scores successfully and changes styles

--- src/modules/start-api.js
+++ src/modules/start-api.js
@@ -12,6 +12,23 @@ const state = { scoresCount: 0 };
 // DOM elements
 const getAllScoresBtn = document.getElementById('refresh-btn');
 const scoresContainerUl = document.getElementById('scores');
+const addNewScoreForm = document.getElementById('add-score-form');
+const submitBtn = document.getElementById('submit');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+
+const showSuccessBtn = () => {
+  nameInput.value = '';
+  scoreInput.value = '';
+  submitBtn.style.backgroundColor = '#000';
+  submitBtn.style.color = '#fff';
+  submitBtn.value = 'Success!!';
+  setTimeout(() => {
+    submitBtn.style.backgroundColor = '#fff';
+    submitBtn.style.color = '#000';
+    submitBtn.value = 'Submit';
+  }, 1000);
+};
 
 const createNewGame = (gameName) => async () => {
   const gameData = await createGame(gameName);
@@ -22,14 +39,12 @@ const createNewGame = (gameName) => async () => {
   endpoints.scores = `${endpoints.games}${game.id}/scores/`;
 };
 
-const submitNewScore = async (user, score) => {
-  await createScore(user, score);
-};
-
 const showAllScores = async () => {
   const scores = await getScores();
+  // if there are new scores display them
   if (scores.length > state.scoresCount) {
     state.scoresCount = scores.length;
+    scoresContainerUl.innerHTML = '';
     scores.forEach((score) => {
       const scoreEle = document.createElement('li');
       scoreEle.innerText = `${score.user}: ${score.score}`;
@@ -38,14 +53,19 @@ const showAllScores = async () => {
   }
 };
 
+const submitNewScore = async (eve) => {
+  eve.preventDefault();
+  const name = nameInput.value;
+  const score = scoreInput.value;
+  await createScore(name, score);
+  // set styles and clear field after successfull submit
+  showSuccessBtn();
+};
+
 const startApp = async () => {
   window.onload = createNewGame('Tic Tac Toe, Find That Foe!');
   await createNewGame('Tic Tac Toe, Find That Foe!')();
-  await submitNewScore('Henry', 100);
-  await submitNewScore('Ron', 20);
-  await submitNewScore('Hero', 30);
-  await submitNewScore('Akif', 50);
-  await submitNewScore('Tafara', 10);
+  addNewScoreForm.onsubmit = submitNewScore;
   getAllScoresBtn.onclick = showAllScores;
 };
 
