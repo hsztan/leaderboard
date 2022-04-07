e79db759622080c6b505c0e6eb476e5f7c251837
Henry Nawrocki <hdev@sztanski.com>
2022-04-07 18:34:25 -0500

    Fix style linters

--- src/modules/globals.js
+++ src/modules/globals.js
@@ -1,5 +1,4 @@
-const baseUrl =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 const endpoints = {
   games: `${baseUrl}games/`,
